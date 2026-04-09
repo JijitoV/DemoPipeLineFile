3a6aae213ebce1d13dfc74076c5238549184b40b
JijitoV <jijitovijitus@gmail.com>
2026-04-09 21:43:43 +0530

    adding other two files

---
+++ List.py
@@ -0,0 +1,8 @@
+num = [1,4,6,10,2,1,25]
+string = ['Hi','Hello','How are you?','Hi']
+print(num)
+mix = [num , string]
+print(mix)
+print(mix[1][-1])
+num.extend([100,25,20,30,34])
+print(num)
---
+++ Tuple.py
@@ -0,0 +1,10 @@
+tup = (23,24,24,16,9)
+print(type(tup))
+print(tup.count(24))
+print(tup.index(16))
+x = tup[0]
+y = tup[2]
+print(x+y)
+tup1 = (34, 'Jijito', 23.4, [13,29,17,35])
+print('Jijito' in tup1)
+print(17 in tup1)

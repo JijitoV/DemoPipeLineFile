b45ce744ad4980fde2f33968b35a6bcd97264ee9
JijitoV <jijitovijitus@gmail.com>
2026-04-23 12:04:49 +0530

    Merging Dictionaries file

---
+++ DictionariesAndSet.py
@@ -0,0 +1,9 @@
+set1 = {25, 'Jijito', 25, 20.4, 'Navin'}
+print(set1)
+dict1 = {
+    'Name' : 'jijito', 
+    'Age' : 26, 
+    'Language' : {'tamil', 'english', 'malayalam'}, 
+    'Programming' : {'Frontend' : ['html', 'css'], 'Backend' : {'java', 'python'}, 'Tool' : {'java': {'eclipse','vscode'}}  }
+    }
+print(dict1['Programming']['Tool']['java'])
